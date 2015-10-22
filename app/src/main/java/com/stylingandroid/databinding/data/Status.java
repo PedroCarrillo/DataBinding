800d449736a313c3be24f57593e578b12a187ec8
Pedro Carrillo <pcarrillo@belatrixsf.com>
2015-10-22 14:53:23 -0500

    data binding images

--- app/src/main/java/com/stylingandroid/databinding/data/Status.java
+++ app/src/main/java/com/stylingandroid/databinding/data/Status.java
@@ -9,12 +9,14 @@ public class Status {
     private final String screenName;
     private final String text;
     private final String imageUrl;
+    private final Status quotedStatus;
 
-    public Status(@NonNull String name, @NonNull String screenName, @NonNull String text, @NonNull String imageUrl) {
+    public Status(@NonNull String name, @NonNull String screenName, @NonNull String text, @NonNull String imageUrl, Status quotedStatus) {
         this.name = name;
         this.screenName = screenName;
         this.text = text;
         this.imageUrl = imageUrl;
+        this.quotedStatus = quotedStatus;
     }
 
     public String getName() {
@@ -32,4 +34,13 @@ public class Status {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    public Status getQuotedStatus() {
+        return quotedStatus;
+    }
+
+    public boolean hasQuotedStatus() {
+        return quotedStatus != null;
+    }
+
 }
